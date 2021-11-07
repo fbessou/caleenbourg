a85cea726ad9e9a809d52327b1859638e6b2198d
Frank Bessou <fbessou@free.fr>
2021-11-07 18:48:57 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Caleenbourg
+===========
+
+Caleenbourg is a command line utility producinc word sequences which sound like an input sentence.
+
+For the moment only french language is supported through the ["Open Lexicon" database](http://www.lexique.org/).
+
+## Running it:
+
+```
+python3 -m caleenbourg "cale en bourg "
+
+```
---
+++ caleenbourg/__main__.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+from pathlib import Path
+import random
+import sys
+
+
+def text_to_phon(dictionnary, text):
+    text = text.replace("'", " ")
+    phon = ""
+    for word in text.split():
+        w_phon = dictionnary.get(word, word)
+        phon += w_phon
+    return phon
+
+
+def phon_to_text(dictionnary_inv, phon, text=""):
+    if not phon:
+        print(f"*{text}")
+        return
+
+    shuffled_keys = list(dictionnary_inv.keys())
+    random.shuffle(shuffled_keys)
+    for word_phon in shuffled_keys:
+        orths = dictionnary_inv[word_phon]
+        if phon.startswith(word_phon):
+            random.shuffle(orths)
+            orth = orths[0]
+            phon_to_text(dictionnary_inv, phon[len(word_phon):], text + " " + orth)
+    else:
+        pass  # print(text)
+
+
+def main(text):
+    with (Path(__file__).parent / "data" / "dict.tsv").open() as f:
+        dictionnary = dict()
+        dictionnary_inv = defaultdict(list)
+        for ortho, phon in [line.split("\t")[0:2] for line in f.readlines()]:
+            dictionnary[ortho] = phon
+            if len(ortho) > 1 or ortho not in "zrtpqsdfghjklmwxcvbniuoe":
+                dictionnary_inv[phon].append(ortho)
+
+    phon = text_to_phon(dictionnary, text)
+    print(phon)
+    phon_to_text(dictionnary_inv, phon)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1].lower())
